965e8b43bcc73d6fb4a4c9be8076250b679b8d68
Bohdan <nilsrondhuis@ukr.net>
2022-12-19 18:28:40 +0200

    first commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,16 +3,27 @@ import ContactForm from 'components/ContactForm/ContactForm';
 import ContactList from 'components/ContactList/ContactList';
 import Filter from 'components/Filter/Filter';
 
+const LS_KEY = 'contacts';
+
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(LS_KEY));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(pP, pS) {
+    const { contacts } = this.state;
+    if (pS.contacts !== this.state.contacts) {
+      localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+    }
+  }
 
   deleteContact = contactId => {
     this.setState(prevState => ({
